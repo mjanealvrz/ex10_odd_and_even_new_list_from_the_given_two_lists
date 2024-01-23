2f0145dddb112ee74ebf4282a8bdf37bd9b0fc47
MJ Gillado <mjgillado0@gmail.com>
2024-01-24 02:52:11 +0800

    Created pseudocode

--- new_list_from_two_given_two_list.py
+++ new_list_from_two_given_two_list.py
@@ -1,2 +1,16 @@
 # Create a new list from two list using the following condition
-#Given two list of numbers, write a program to create a new list such that the new list should contain odd numbers from the first list and even numbers from the second list.
+#Given two list of numbers, write a program to create a new list such that the new list should 
+#contain odd numbers from the first list and even numbers from the second list.
+
+
+# Pseudocode
+
+# Initialize  the given list
+# Initialize empty list and store results
+# Using for loop iterate the list number 1
+    # Check if the number is odd
+    # If it's odd, append it to the result_list
+# Using for loop iterate the list number 2
+    # Check if the number is even
+    # If it's even, append it to the result_list
+# Print results 
