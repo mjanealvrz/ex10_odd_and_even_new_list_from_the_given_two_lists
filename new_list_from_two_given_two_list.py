6f64a41acf4d7e048bb9e3179d428b1b245af13d
MJ Gillado <mjgillado0@gmail.com>
2024-01-24 02:27:39 +0800

    Added program statement

---
+++ new_list_from_two_given_two_list.py
@@ -0,0 +1,2 @@
+# Create a new list from two list using the following condition
+#Given two list of numbers, write a program to create a new list such that the new list should contain odd numbers from the first list and even numbers from the second list.
